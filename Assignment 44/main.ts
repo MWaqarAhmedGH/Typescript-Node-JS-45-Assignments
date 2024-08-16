5edd72ecedc25dc9682dc1bd4c8913fe655fe4cb
M Waqar Ahmed <fromwaqar@gmail.com>
2024-08-16 19:24:23 +0500

    first commit

--- Assignment 44/main.ts
+++ Assignment 44/main.ts
@@ -4,5 +4,12 @@
 parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that is 
 being ordered. Call the function three times, using a different number of arguments each time.  */
 
-
+function makeSandwich(...items: string[])  //  Making a function called makeSandwich
+{
+    console.log(`Making a sandwich with: ${items.join(", ")}.`);
+}
+  
+makeSandwich("Veggie", "Mayo Cheese");  //  Calling a function makeSandwich for 1st time
+makeSandwich("Chicken", "French Dip", "");  //  Calling a function makeSandwich for 2nd time
+makeSandwich("Tomato", "Grilled Cheese", "Club Sandwich", "Cuban Sandwich");  //  Calling a function makeSandwich for 3rd time
 
